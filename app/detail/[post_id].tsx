8a9dd9f0cc4b1a0dd55f903426452c6edb69d332
Michael <ccmichael922@gmail.com>
2025-07-27 15:31:32 +0800

    2025.7.27

--- app/detail/[post_id].tsx
+++ app/detail/[post_id].tsx
@@ -1,9 +1,10 @@
 import { useLocalSearchParams } from 'expo-router';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Detail = () => {
   const {post_id} = useLocalSearchParams()
-  
+  const [post, setPost] = useState<any>(null)
   return (
     <View>
       <Text>Hello, RNFES!</Text>
